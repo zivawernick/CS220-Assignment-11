f941ac12c5af4a3622007636a284cb47dff92b02
Samantha Huhgson <shughson@clarku.edu>
2018-03-25 22:49:58 -0400

    Add files via upload
    Fixed most of the new customer - the query is still not 100% there (the input of the params is off I think)

--- musicdb.py
+++ musicdb.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Mar 18 18:24:00 2018
 
-@author: your name here
+@author: Lu Liu
 """
 import sqlite3 as db
 
@@ -41,12 +41,66 @@ class MusicDB:
         return(result)
     
     def searchEmployee(self, name):
-        cur = self.conn.cursor();
+        cur = self.conn.cursor()
         params = (name,)
         cur.execute("SELECT * FROM employees WHERE LastName like ?", params)
         result = cur.fetchall()
         return(result)
         
+    def findnetID(self, IdType, table):
+        cur = self.conn.cursor()
+        cur.execute("Select MAX(" + IdType + " + 1) from " + table)
+        results = cur.fetchone
+        return(results)
+
+    def createCustomer(self, FirstName, LastName, CustomerId, Address, City,
+                  State, Country, PostalCode, Phone, Email):
+        #Create a cursor object to execute queries and review results
+        cur = self.conn.cursor();
+        
+        #Create the python tuple with all the values
+        params = (FirstName, LastName, CustomerId, Address, City,
+                  State, Country, PostalCode, Phone, Email, '4')
+        # Run a query
+        #this is not working yet but I am not sure how to fix it - in SQLite this is the setup for the querry 
+        cur.execute("Insert into customers (FirstName, LastName, CustomerId, Address, City, State, Country, PostalCode, Phone, Email, SupportRepId) Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
+        #commit the changes 
+        self.conn.commit()
+            
+    def createEmployee(self):
+        # Create a cursor object to execute queries and review results
+        cur = self.conn.cursor();
+        
+        # Create the python tuple with all the values
+        params = (EmployeeId, LastName, FirstName, Title, ReportsTo, BirthDate,
+                  HireDate, Address, City, State, Country, PostalCode, Phone,
+                  Fax, Email)
+        
+        # Run a query
+        cur.execute("Select EmployeeId, LastName, FirstName, Title, ReportsTo,"
+                    "BirthDate, HireDate, Address, City, State, Country," 
+                    "PostalCode, Phone, Fax, Email From employees", params)
+
+        # Fetch all the results
+        result = cur.fetchall()
+        for row in results:
+            EmployeeId, LastName, FirstName, Title, ReportsTo, BirthDate,
+            HireDate, Address, City, State, Country, PostalCode, Phone,
+            Fax, Email = row
+                  
+            print("Employee ID: %d" % EmployeeId)
+            print("%s %s" % (FirstName, LastName))
+            print("Title: %s" % Title)
+            print("Reports To: %s" % ReportsTo)
+            print("Birth Date: %s" % BirthDate)
+            print("Hire Date: %s" % HireDate)
+            print("Address: %s %s %s %s %s" % (Address, City, State,
+                                                     Country, PostalCode))
+            print("Phone: %s" % Phone)
+            print("Fax: %s" % Fax)
+            print("Email: %s" % Email)
+            print()
+            
     def updateEmployeeEmail(self, name, empID, email):
         #create a cursor object 
         cur = self.conn.cursor();
@@ -66,4 +120,3 @@ class MusicDB:
         cur.execute("Update customers Set email = ? Where LastName = ? and CustomerId = ?", params)
         #commit the changes 
         self.conn.commit()
-        
